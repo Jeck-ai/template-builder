1673019be2557e929891a316d8c6f02ec5a339e6
Chad Carlson <chadwcarlson@gmail.com>
2025-02-21 16:17:02 -0500

    Compare validation updates.

---
+++ validate.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+cd upsun_config_validator
+
+python3 -m venv venv
+source venv/bin/activate
+
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Run the validation
+clear
+DIRECTORY="$(pwd)"
+parentdir="$(dirname "$DIRECTORY")"
+UPDATED_DIR="$parentdir/templates"
+
+find "$UPDATED_DIR" -mindepth 1 -maxdepth 1 -type d | while read -r dir; do
+    printf "\n*******************************************************************************************************\n\n"
+    printf "* Processing template: $dir\n\n"
+
+    # In-house validation
+    python test_individual.py "$dir/files"
+
+    # Built-in CLI validation
+    cd "$dir/files"
+    upsun app:config-validate
+    cd $DIRECTORY
+
+done
